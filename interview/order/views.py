1d27d9cc5a849cd02545f3152fa15541af5d5599
Ben Olsen <benjamin.olsen@pluto.tv>
2024-11-04 22:25:12 -0700

    challenge 2 done

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
 # Create your views here.
+class OrderDeactivateView(APIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        order = self.queryset.get(id=kwargs['id'])
+        order.is_active = False
+        serializer = self.serializer_class(order, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+        serializer.save()
+
+        return Response(serializer.data, status=200)
+
+
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    
+
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
